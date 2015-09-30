4f510faf82a8a9878a21b921b76cc4a787940107
maturro <mavileswlf@gmail.com>
2015-09-29 23:44:52 -0300

    Casi completo

--- src/Lista.java
+++ src/Lista.java
@@ -37,15 +37,72 @@ public class Lista {
     public cola moveToQueue()
     {
         cola c = new cola();
+        cola c2 = new cola();
+        cola c3 = new cola();
+        cola c4 = new cola();
+        int elemento ; 
+        int prori = 0;
+        
         Nodo aux = primero;
         Numero n ;
         while( aux != null ){
             n = aux.getNumero();
+          
+            if (n.getPrioridad() == 1)
+            {
+                c.encolar(n.getN());
+                c2.encolar(n.getPrioridad());
+            }
             
-            c.encolar(n);
+            while( c.size() > 0 ){
+            
+            
+            }
             
             aux = aux.getSiguiente();
-        }
+      }
+            
+            
+            
+            
+            
+            
+//            if (n.getPrioridad() >1)
+//            {
+//                while (n.getPrioridad() <= 2)
+//                {
+//                    
+//                    if ((int)c2.desencolar() == n.getPrioridad())
+//                        prori = n.getPrioridad();
+//                    else
+//                    {
+//                    elemento = (int)c2.desencolar();
+//                    
+//                        
+//                    prori = elemento;
+//
+//                        c4.encolar(elemento);
+//                        elemento = (int)c.desencolar();
+//                        c3.encolar(elemento);
+//                    }
+//                }
+//                
+//                c.encolar(n.getN());
+//                c2.encolar(n.getPrioridad());
+//                
+//                for (int i = 0 ; i <= c3.size() ; i++) {
+//                    
+//                    elemento = (int)c3.desencolar();
+//                    c.encolar(elemento);
+//                    elemento = (int)c4.desencolar();
+//                    c2.encolar(elemento);
+//                }
+//                
+//            }
+//            
+//            
+//            aux = aux.getSiguiente();
+//        }
         
         return c;
     }
